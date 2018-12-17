3532afafc11761b93a8c8654b8ac60acc91db557
chen <1773420864@qq.com>
2018-12-17 19:50:41 +0800

    "this is maven-java"

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -7,4 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public void add() {
+        System.out.println("我是大美女");
+    }
 }
