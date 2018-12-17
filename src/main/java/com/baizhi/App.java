64f35f2c4a18a3a002d9bd865dd04e20fdafe7b0
chenmengnan2 <45877367+chenmengnan2@users.noreply.github.com>
2018-12-17 19:55:57 +0800

    Update App.java

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -5,6 +5,7 @@ package com.baizhi;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!......");
+        
     }
 }
